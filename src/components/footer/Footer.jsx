62fd07227922f36f72efabcc82d1a05378023da8
Ning Ma <ningma905@gmail.com>
2022-10-01 19:44:38 +0100

    feat: footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import "./footer.css"
+
+export default function Footer() {
+	return (
+		<footer className="footer">
+			<div className="fLists">
+				<ul className="fList">
+					<li className="fList-item">Countries</li>
+					<li className="fList-item">Regions</li>
+					<li className="fList-item">Cities</li>
+					<li className="fList-item">Districts</li>
+					<li className="fList-item">Airports</li>
+					<li className="fList-item">Hotels</li>
+				</ul>
+				<ul className="fList">
+					<li className="fList-item">Homes </li>
+					<li className="fList-item">Apartments </li>
+					<li className="fList-item">Resorts </li>
+					<li className="fList-item">Villas</li>
+					<li className="fList-item">Hostels</li>
+					<li className="fList-item">Guest houses</li>
+				</ul>
+				<ul className="fList">
+					<li className="fList-item">Unique places to stay </li>
+					<li className="fList-item">Reviews</li>
+					<li className="fList-item">Unpacked: Travel articles </li>
+					<li className="fList-item">Travel communities </li>
+					<li className="fList-item">Seasonal and holiday deals </li>
+				</ul>
+				<ul className="fList">
+					<li className="fList-item">Car rental </li>
+					<li className="fList-item">Flight Finder</li>
+					<li className="fList-item">Restaurant reservations </li>
+					<li className="fList-item">Travel Agents </li>
+				</ul>
+				<ul className="fList">
+					<li className="fList-item">Customer Service</li>
+					<li className="fList-item">Partner Help</li>
+					<li className="fList-item">Careers</li>
+					<li className="fList-item">Sustainability</li>
+					<li className="fList-item">Press center</li>
+					<li className="fList-item">Safety Resource Center</li>
+					<li className="fList-item">Investor relations</li>
+					<li className="fList-item">Terms & conditions</li>
+				</ul>
+			</div>
+			<div className="fText">Copyright © 2022 Booking.</div>
+		</footer>
+	)
+}
