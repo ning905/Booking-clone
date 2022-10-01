e76ae62e9f0a62c40d97839083bdc1684c8a7240
Ning Ma <ningma905@gmail.com>
2022-10-01 16:52:17 +0100

    feat: nav bar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="nav-container">
+                <span className="logo">Booking</span>
+                <div className="nav-items">
+                    <button className="nav-button">Register</button>
+                    <button className="nav-button">Login</button>
+
+                </div>
+
+
+
+            </div>
+        </div>
+    )
+}
