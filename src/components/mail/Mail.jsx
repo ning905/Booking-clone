a960d77d88c9d86d0292dffcfd01095d55325df3
Ning Ma <ningma905@gmail.com>
2022-10-01 19:44:48 +0100

    feat: mail

---
+++ src/components/mail/Mail.jsx
@@ -0,0 +1,16 @@
+import "./mail.css"
+
+export default function Mail() {
+	return (
+		<div className="mail">
+			<h1 className="mail-title">Save time, save money!</h1>
+			<span className="mail-desc">
+				Sign up and we'll send the best deals to you
+			</span>
+			<div className="mail-input-container">
+				<input type="text" placeholder="Your Email" />
+				<button>Subscribe</button>
+			</div>
+		</div>
+	)
+}
